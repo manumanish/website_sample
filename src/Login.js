c958d1a264b011a0904ab5984a1b4c994cfb419c
Manish Kumar <gangishettymanish9@gmail.com>
2023-05-27 15:53:26 +0530

    updated site

--- src/Login.js
+++ src/Login.js
@@ -1,48 +1,74 @@
 import React, { useState } from 'react';
+import { Routes,Route,BrowserRouter,useNavigate,useLocation   } from "react-router-dom";
+import { Dashboard } from "./Dashboard"
 
-const Login = () => {
+
+function useAuth(username) {
+  console.log(username);
+  if(username === "pradeep"){
+    return true;
+  }
+  return false;
+  
+  
+}
+
+const Private = () => <FormComponent />;
+
+function PrivateOutlet() {
+  const location = useLocation();
+  const username = location.state?.username || ''; 
+  const auth = useAuth(username);
+  console.log("inside private")
+  return auth ? <Dashboard /> : <FormComponent /> ;
+}
+
+function FormComponent() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
-
   const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+      setPassword(event.target.value);
   };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Perform login logic here, such as validating credentials or making an API request
-    console.log('Username:', username);
-    console.log('Password:', password);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(username);
+    console.log("inside");
+    navigate('/Dashboard', { state: { username } });
+      
   };
-
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={handleUsernameChange}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="text" name="email" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
-};
+}
 
-export default Login;
+function App() {
+  return (
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<FormComponent />} />
+        <Route path="/Dashboard" element={<PrivateOutlet/>}>
+          <Route path="" element={<Private />} />
+        </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
